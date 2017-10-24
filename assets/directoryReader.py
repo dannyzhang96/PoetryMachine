2bf01436a1203bdf3b13730184a75f3e5a57fc42
Danny Zhang <zhangdanny96@gmail.com>
2017-10-23 21:15:59 -0400

    added poem and poet lists

--- assets/directoryReader.py
+++ assets/directoryReader.py
@@ -1,9 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
-f = open('poetList.txt', 'w+')
-path = "./collection/"
-dirs = os.listdir(path)
-for fl in dirs:
-    print(fl)
-    f.write(fl + ", ")
-f.close()
+
+def getPoetList(): 
+    f = open('poetList.txt', 'w')
+    path = './collection/'
+    dirs = os.listdir(path)
+    for poet in dirs:
+        if not poet.startswith('.'):
+            f.write('"' + poet + '", ')
+    f.close()
+    with open('poetList.txt', 'rb+') as f:
+        f.seek(-2, os.SEEK_END)
+        f.truncate()
+
+def getPoemList():
+    f = open('poemList.txt', 'w')
+    path = './collection/'
+    for poet in os.listdir(path):
+        if not poet.startswith('.'):
+            dirs = os.listdir(path + poet)
+            for poem in dirs:
+                if not poem.startswith('.'):
+                    f.write('"' + poem + '", ')
+    f.close()
+    with open('poemList.txt', 'rb+') as f:
+        f.seek(-2, os.SEEK_END)
+        f.truncate()
