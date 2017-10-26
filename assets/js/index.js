9d74b1bb454287b5b9107de705de71176dbd73dc
Danny Zhang <zhangdanny96@gmail.com>
2017-10-26 10:47:15 -0400

    wrote random poem generating algorithm

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,15 @@
 $(document).ready(
     function generatePoem() {
-        document.getElementById('poem').innerHTML = "random poem";
+        var poemID = Math.floor(Math.random() * 12529)
+        var request = new XMLHttpRequest();
+        request.open("GET", "assets/collection/" + poemID + ".json", false);
+        request.send(null)
+        var my_JSON_object = JSON.parse(request.responseText);
+        $('#poemTitle').html(my_JSON_object.title);
+        $('#poemAuthor').html(my_JSON_object.author);
+        for (i = 0; i < my_JSON_object.text.length; i ++) {
+            $('#poemText').append('<p>' + my_JSON_object.text[i] + '</p>');
+        }
 });
 
 
